009f5a925768583112b13ff894268385f5cad999
Nikoloman <hectorvel52@gmail.com>
2023-01-31 13:24:18 -0600

    A simple program that prints a variable Heck, C is harder than I thought

---
+++ Week 1 - C/helloWorld.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // Standard input output
+
+int main (void){
+    char strName[] = "Héctor"; // Turns out, there aren't such things as string in C
+    printf("Hello %s\n", strName); // Printing a variable with printf
+}
