06f1c9b9c8d64176cdfc2db2b79447f5afb748fd
Nikoloman <hectorvel52@gmail.com>
2023-01-31 16:10:27 -0600

    A program that uses loops to print "meow"

---
+++ Week 1 - C/meow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void){
+
+    // While loop
+    int i = 0;
+    while (i < 3){ 
+        printf("Meow /ᐠ.ꞈ.ᐟ\\ \n"); // A cute cat
+        i++; // Increment
+    }
+    printf("\n");
+
+    // For loop
+    for (int i = 0; i < 3; i++){
+        printf("Meow (=^ω^=) \n");
+    }
+}
