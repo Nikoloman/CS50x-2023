cc87b4f5d1c4dc878e6f255a827599d0413b4b83
Nikoloman <hectorvel52@gmail.com>
2023-02-08 11:24:37 -0600

    A more optimized code

--- Week 2 - Arrays/Alphabetical exercise.c
+++ Week 2 - Arrays/Alphabetical exercise.c
@@ -6,33 +6,21 @@ If not, print "No".
 
 #include <stdio.h>
 #include <string.h>
-#include <stdbool.h>
 
 int main(){
     char string[20];
 
     printf("Write the sequence of letters (limit 20): ");
     scanf("%s", string);
-    
+
     int stringLength = strlen(string);
-    char c = string[0];
-    bool ordered;
 
-    for (int i = 0; i < stringLength; i ++){
-        if (c <= string[i]){
-            c = string[i];
-            ordered = true;
-        }
-        else{
-            ordered = false;
-            break;
+    for (int i = 0; i < stringLength - 1; i ++){
+        if (string[i] > string[i + 1]){
+            printf("No");
+            return 0;
         }
     }
 
-    if (ordered){
-        printf("Yes");
-    }
-    else{
-        printf("No");
-    }
+    printf("Yes");
 }
