eaa96c736ab2a497f3bba7351e17eda8ec2c04e0
Nikoloman <hectorvel52@gmail.com>
2023-03-06 12:53:11 -0600

    Few examples about loops in Python

---
+++ Week 6 - Python/loops.py
@@ -0,0 +1,15 @@
+# While loop
+i = 0
+while i < 3:
+    print("Meow")
+    i += 1 # There's no such thing as i++ in python :(
+print()
+
+# For loop
+for i in range(3): # in python we need to put a function called range
+    print("Meow")
+print()
+
+# Infinite loop
+while True:
+    print("Meow")
