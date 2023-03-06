1438e867eb55cd6423b831dd4c1b17d33d169ecc
Nikoloman <hectorvel52@gmail.com>
2023-03-06 17:16:18 -0600

    A simple program with a way to swap values really fast

---
+++ Week 6 - Python/swap.py
@@ -0,0 +1,8 @@
+x = int(input("Write the value of x: "))
+y = int(input("Write the value of y: "))
+
+# SWAP!!!1!1!11!!!
+x, y = y, x
+
+print(f"The value of x is now: {x}")
+print(f"The value of y is now: {y}")
