6c3f3e5aa9e0e40faa3d804f4d16a0e30bc6be98
Nikoloman <hectorvel52@gmail.com>
2023-03-06 13:06:31 -0600

    A simple calculator in Python

---
+++ Week 6 - Python/calculator.py
@@ -0,0 +1,7 @@
+x = int(input("Write a number: "))
+y = int(input("Write another number: "))
+
+print(f"{x} + {y} = {x + y}")
+print(f"{x} - {y} = {x - y}")
+print(f"{x} * {y} = {x * y}")
+print(f"{x} / {y} = {(x / y):.3f}") # Shows only 3 digits after the decimal point
